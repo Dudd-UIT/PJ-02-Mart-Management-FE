e60ebf1a872301e3b2011cfddbc9ae5a7f214365
nghgi <22520357@gm.uit.edu.vn>
2024-10-31 08:15:15 +0700

    fix: side nav

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -37,7 +37,6 @@ export default function Sidebar() {
       className={`sidebar col-md-2 col-sm-4 ${isPinned ? 'pinned' : ''}`}
       id="side_nav"
       style={{
-        height: '100vh',
         width: isPinned ? '230px' : '60px',
         transition: 'width 0.3s',
       }}
