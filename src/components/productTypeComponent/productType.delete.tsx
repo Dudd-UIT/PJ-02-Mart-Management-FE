91e47fda7b338e4b3bdf3d99d1a6eb836def036c
ngxquang <baophong2401@gmail.com>
2024-11-05 17:21:03 +0700

    feat(product-type): add manage Product Type

---
+++ src/components/productTypeComponent/productType.delete.tsx
@@ -0,0 +1,55 @@
+import { handleDeleteProductTypeAction } from '@/services/productTypeServices';
+import { ProductType } from '@/types/productType';
+import { Button, Modal } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+
+const DeleteProductTypeModal = (props: DeleteModalProps<ProductType>) => {
+  const {
+    isDeleteModalOpen,
+    setIsDeleteModalOpen,
+    data: productTypeData,
+    onMutate,
+  } = props;
+  const handleCloseDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleDeleteProductType = async () => {
+    const res = await handleDeleteProductTypeAction(productTypeData?.id);
+    if (res?.data) {
+      handleCloseDeleteModal();
+      onMutate();
+      toast.success('Xóa loại sản phẩm thành công');
+    } else {
+      toast.error('Lỗi xóa loại sản phẩm');
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        show={isDeleteModalOpen}
+        onHide={handleCloseDeleteModal}
+        size="lg"
+        backdrop={'static'}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Xác nhận</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>Bạn có chắc muốn xóa loại sản phẩm {productTypeData?.name}?</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDeleteModal}>
+            Hủy
+          </Button>
+          <Button variant="danger" onClick={handleDeleteProductType}>
+            Đồng ý
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default DeleteProductTypeModal;
